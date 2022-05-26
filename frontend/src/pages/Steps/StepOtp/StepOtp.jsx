bacdcbdbeaef5ed72db38703d4b98c00cc509bb6
unknown <gs27071994@gmail.com>
2022-05-26 16:38:01 +0530

    firstcommit

---
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StepOtp = ({onNext}) => {
+  return (
+      <>
+    <div>StepOtp</div>
+    <button onClick={onNext}>Next</button>
+      </>
+    
+  )
+}
+
+export default StepOtp;
---
+++ frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StepPhoneEmail = ({onNext}) => {
+  return (
+      <>
+      <div>Phone or Email component</div>
+      <button onClick={onNext}>Next</button>
+      </>
+    
+  )
+}
+
+export default StepPhoneEmail;
